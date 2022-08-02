9cb7057a8720d3e2ae04758377266b83cc840136
Ahmed Alli <muniirhaddadi1@gmail.com>
2022-08-02 14:08:51 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var employee = {};
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    var newEmployee={};
+            newEmployee['name'] = 'Sam';
+            newEmployee['streetAddress']= '11 Broadway';
+        return newEmployee;
+      }
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee['name'] = 'Sam';
+    employee['streetAddress']= '12 Broadway';
+    return employee
+  }
+
+
+  function  deleteFromEmployeeByKey(employee, key){
+    var newEmployee={}; 
+            newEmployee['streetAddress']= '11 Broadway';
+            delete newEmployee['streetAddress']
+        return newEmployee;
+  }
+  function destructivelyDeleteFromEmployeeByKey(employee, key){
+    employee['name'] = 'Sam';
+    employee['streetAddress']= '12 Broadway';
+    delete employee['name'];
+    return employee
+  }
